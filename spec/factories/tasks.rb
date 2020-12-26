2e2f11099c74bc22adfe964884792364ee63ae34
sosome2020 <letebelebrightsosome@gmail.com>
2020-12-26 19:20:47 +0900

    wrote the test: system & model(rpec)

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :task do
+    # Please change the following contents according to the column name actually created
+    task_name { 'test_title' }
+    task_detail { 'test_content' }
+  end
+end
