93c0f489e66fdb7e2a23db4d91ebbe7c2d34a8dc
sosome2020 <letebelebrightsosome@gmail.com>
2021-01-17 21:18:20 +0900

    fixed the sorting of the priority

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   validates :task_name,  presence: true
   validates :task_detail,  presence: true
   validates :expiration_date,  presence: true
-  
+  validates :priority, presence: true , numericality: { only_integer: true, greater_than: -1, less_than: 4 }
 end
