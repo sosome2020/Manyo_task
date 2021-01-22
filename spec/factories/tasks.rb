8cf3d4b26c0813d8aa165a88e79f616b6137a540
sosome2020 <letebelebrightsosome@gmail.com>
2021-01-22 13:19:20 +0900

    added models & tasks system spec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,23 @@
 FactoryBot.define do
-  factory :task do
-    # Please change the following contents according to the column name actually created
-    task_name { 'test_title' }
-    task_detail { 'test_content' }
-    expiration_date { '18/01/2021' }
-    priority { 3 }
-    status { 'Started' }
+  factory :task_2 do
+  task_name { 'test_title' }
+  task_detail { 'test_content' }
+  expiration_date { '2021-09-29'}
+  user
   end
+
+  factory :second_task_2, class: Task do
+  task_name { 'second' }
+  task_detail { 'second one' }
+  expiration_date { '2020-01-07'}
+  user
+  end
+
+  factory :third_task_2, class: Task do
+  task_name { 'third' }
+  task_detail { 'third one' }
+  expiration_date { '2022-11-29'}
+  user
+  end
+
 end
