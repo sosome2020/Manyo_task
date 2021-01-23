5ccc6c69b408384579282736391cfa106906df62
sosome2020 <letebelebrightsosome@gmail.com>
2021-01-23 20:06:37 +0900

    fixed the admin role problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   validates:email,presence:true,length:{maximum: 255 }, format: { with:/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, presence: true, length: {minimum: 6 }, on: :create
+  validates :password, presence: true, length: {minimum: 6 }
   has_many :tasks, dependent: :destroy
   before_destroy :must_not_destroy_last_admin
   private
